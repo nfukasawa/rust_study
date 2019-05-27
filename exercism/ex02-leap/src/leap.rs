8f5dab19958b72fe827b9e2174c39748f52cc29b
nfukasawa <get.it.on.fuka@gmail.com>
2019-05-27 19:49:54 +0900

    add other impl.

--- exercism/ex02-leap/src/leap.rs
+++ exercism/ex02-leap/src/leap.rs
@@ -1,4 +1,4 @@
-pub fn is_leap(year: u64) -> bool {
+pub fn is_leap1(year: u64) -> bool {
     match year {
         year if year % 400 == 0 => true,
         year if year % 100 == 0 => false,
@@ -7,12 +7,30 @@ pub fn is_leap(year: u64) -> bool {
     }
 }
 
+pub fn is_leap2(year: u64) -> bool {
+    if year % 400 == 0 {
+        true
+    } else if year % 100 == 0 {
+        false
+    } else if year % 4 == 0 {
+        true
+    } else {
+        false
+    }
+}
+
 #[test]
-fn name() {
-    assert!(!is_leap(1900));
-    assert!(is_leap(2000));
-    assert!(!is_leap(2001));
-    assert!(!is_leap(2002));
-    assert!(!is_leap(2003));
-    assert!(is_leap(2004));
+fn test_is_leap() {
+    let cases = vec![
+        (1900, false),
+        (2000, true),
+        (2001, false),
+        (2002, false),
+        (2003, false),
+        (2004, true),
+    ];
+    for (year, wants) in cases {
+        assert_eq!(wants, is_leap1(year));
+        assert_eq!(wants, is_leap2(year));
+    }
 }
--- exercism/ex02-leap/src/main.rs
+++ exercism/ex02-leap/src/main.rs
@@ -2,6 +2,6 @@ mod leap;
 
 fn main() {
     for y in 2000..2100 {
-        println!("{} -> {}", y, leap::is_leap(y));
+        println!("{} -> {}", y, leap::is_leap1(y));
     }
 }
