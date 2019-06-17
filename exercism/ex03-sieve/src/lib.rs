ac6ffd11cf68bf51a257ecf5710600f895a19653
nfukasawa <get.it.on.fuka@gmail.com>
2019-06-17 21:16:49 +0900

    use take instead of cloned.

--- exercism/ex03-sieve/src/lib.rs
+++ exercism/ex03-sieve/src/lib.rs
@@ -2,7 +2,7 @@ pub fn primes_up_to(limit: i32) -> Vec<i32> {
     let mut xs = (2..limit + 1).collect::<Vec<i32>>();
     let mut ps = Vec::<i32>::new();
 
-    while let Some(p) = xs.first().cloned() {
+    while let Some(&p) = xs.first().take() {
         ps.push(p);
         xs.retain(|&x| x % p != 0);
         if p * p > limit {
