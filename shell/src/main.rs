c69b79ca67f22c6fec56e507012c25848d931c05
nfukasawa <get.it.on.fuka@gmail.com>
2019-11-17 19:27:00 +0900

    impl exec and pipe.

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -1,45 +1,50 @@
 use std::io;
 use std::io::prelude::*;
-
-use std::process;
+use std::process::{Child, Command, Stdio};
 
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let stdin = stdin.lock();
 
-    let stdout = io::stdout();
-    let mut stdout = io::BufWriter::new(stdout.lock());
-
-    let stderr = io::stderr();
-    let mut stderr = stderr.lock();
-
-    prompt(&mut stderr)?;
-    for line in stdin.lines() {
-        let line: String = line?;
-
-        if line.is_empty() {
-            prompt(&mut stderr)?;
-            continue;
-        }
-
-        let cmds = parse_line(&line);
-        println!("{:?}", cmds);
-
-        /*
-        match process::Command::new(line).output() {
-            Ok(res) => {
-                write!(stdout, "{}", std::str::from_utf8(&res.stdout).unwrap())?;
-            }
-            Err(err) => {
-                println!("err :{:?}", err);
-            }
-        }
-        stdout.flush()?;
-        write!(stderr, "\n")?;
-        */
-        prompt(&mut stderr)?;
+    let mut reader = Reader::new(stdin);
+    while let Some(cmds) = reader.cmds() {
+        cmds.exec()?;
     }
+
     Ok(())
+}
+
+#[derive(Debug, Clone)]
+struct Commands {
+    cmds: Vec<Cmd>,
+}
+impl Commands {
+    pub fn new(cmds: Vec<Cmd>) -> Self {
+        Commands { cmds }
+    }
+
+    pub fn exec(self) -> io::Result<()> {
+        let mut children = self
+            .cmds
+            .iter()
+            .map(|cmd| cmd.process())
+            .collect::<Vec<Child>>();
+
+        let mut i = 1;
+        while i < children.len() {
+            let (c1, c2) = children.split_at_mut(i);
+            io::copy(
+                c1[i - 1].stdout.as_mut().unwrap(),
+                c2[0].stdin.as_mut().unwrap(),
+            )
+            .unwrap();
+            i += 1;
+        }
+        for mut child in children {
+            child.wait()?;
+        }
+        Ok(())
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -49,7 +54,6 @@ struct Cmd {
     input: IO,
     output: IO,
 }
-
 impl Cmd {
     pub fn new(cmd: &str) -> Self {
         Self {
@@ -59,6 +63,16 @@ impl Cmd {
             output: IO::Std,
         }
     }
+
+    pub fn process(&self) -> Child {
+        Command::new(&self.cmd)
+            .args(&self.opts)
+            .stdin(self.input.stdin())
+            .stdout(self.output.stdout())
+            .spawn()
+            .expect("failed to run process") // TODO: error
+    }
+
     pub fn add_opt(&mut self, opt: &str) {
         self.opts.push(opt.to_string());
     }
@@ -81,54 +95,89 @@ impl IO {
     pub fn redirect(file: &str) -> IO {
         IO::Redirect(file.to_string())
     }
-}
 
-fn exec_cmds(cmds: &Vec<Cmd>) -> io::Result<()> {
-    Ok(())
-}
-
-fn parse_line(line: &String) -> Vec<Cmd> {
-    // TODO: use perser combinator ?
-    // TODO: parse quotes, etc...
-    // TODO: multi line
-    let mut iter = line.split_ascii_whitespace();
-
-    let mut cmds = Vec::new();
-    let mut pipe = false;
-
-    while let Some(token) = iter.next() {
-        let mut cmd = Cmd::new(token);
-        if pipe {
-            cmd.set_input(IO::Pipe);
-            pipe = false;
+    pub fn stdin(&self) -> Stdio {
+        match self {
+            IO::Pipe => Stdio::piped(),
+            IO::Redirect(file) => Stdio::inherit(), // TODO
+            _ => Stdio::inherit(),
         }
+    }
+
+    pub fn stdout(&self) -> Stdio {
+        match self {
+            IO::Pipe => Stdio::piped(),
+            IO::Redirect(file) => Stdio::inherit(), // TODO
+            _ => Stdio::inherit(),
+        }
+    }
+}
+
+struct Reader<R: BufRead> {
+    reader: R,
+}
+impl<R: BufRead> Reader<R> {
+    pub fn new(reader: R) -> Self {
+        Reader { reader }
+    }
+
+    pub fn cmds(&mut self) -> Option<Commands> {
+        let mut line = String::new();
+        self.prompt().expect("io error");
+
+        // TODO: multi line
+        if let Some(cmds) = match self.reader.read_line(&mut line) {
+            Ok(_) => Some(self.parse_line(&line)),
+            Err(_) => None,
+        } {
+            Some(Commands::new(cmds))
+        } else {
+            None
+        }
+    }
+
+    fn parse_line(&self, line: &String) -> Vec<Cmd> {
+        // TODO: use perser combinator ?
+        // TODO: parse quotes, etc...
+        let mut iter = line.split_ascii_whitespace();
+
+        let mut cmds = Vec::new();
+        let mut pipe = false;
 
         while let Some(token) = iter.next() {
-            match token {
-                "|" => {
-                    cmd.set_output(IO::Pipe);
-                    pipe = true;
-                    break;
-                }
-                ">" => match iter.next() {
-                    Some(file) => {
-                        cmd.set_output(IO::redirect(file));
+            let mut cmd = Cmd::new(token);
+            if pipe {
+                cmd.set_input(IO::Pipe);
+                pipe = false;
+            }
+
+            while let Some(token) = iter.next() {
+                match token {
+                    "|" => {
+                        cmd.set_output(IO::Pipe);
+                        pipe = true;
                         break;
                     }
-                    None => {} // TODO
-                },
-                "<" => match iter.next() {
-                    Some(file) => cmd.set_input(IO::redirect(file)),
-                    None => {} // TODO
-                },
-                _ => cmd.add_opt(token),
+                    ">" => match iter.next() {
+                        Some(file) => {
+                            cmd.set_output(IO::redirect(file));
+                            break;
+                        }
+                        None => {} // TODO
+                    },
+                    "<" => match iter.next() {
+                        Some(file) => cmd.set_input(IO::redirect(file)),
+                        None => {} // TODO
+                    },
+                    _ => cmd.add_opt(token),
+                }
             }
+            cmds.push(cmd);
         }
-        cmds.push(cmd);
+        return cmds;
     }
-    return cmds;
-}
 
-fn prompt<W: io::Write>(out: &mut W) -> io::Result<()> {
-    write!(out, "> ")
+    fn prompt(&self) -> io::Result<()> {
+        write!(io::stderr().lock(), "> ")
+    }
 }
