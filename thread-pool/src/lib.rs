dac64bb88345014e6cf9ad92723b41536da91b75
nfukasawa <get.it.on.fuka@gmail.com>
2019-12-01 16:17:16 +0900

    impl stop.

--- thread-pool/src/lib.rs
+++ thread-pool/src/lib.rs
@@ -1,53 +1,83 @@
+use std::error::Error;
 use std::sync::mpsc;
-use std::sync::Arc;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use std::thread;
+
+pub type ThreadPoolResult = Result<(), Box<dyn Error>>;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::SyncSender<Msg>,
 }
 
 impl ThreadPool {
-    pub fn new(size: usize) -> Self {
-        assert!(size > 0);
-
-        let mut workers = Vec::with_capacity(size);
-        let (sender, receiver) = mpsc::channel();
-
+    pub fn new(queue_size: usize, number_of_threads: usize) -> Self {
+        let mut workers = Vec::with_capacity(number_of_threads);
+        let (sender, receiver) = mpsc::sync_channel(queue_size);
         let receiver = Arc::new(Mutex::new(receiver));
-        for id in 0..size {
-            let receiver = Arc::clone(&receiver);
-            workers.push(Worker::new(id, receiver));
+        for id in 0..number_of_threads {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-
         Self { workers, sender }
     }
 
-    pub fn dispatch<F>(&self, f: F)
+    pub fn dispatch<F>(&self, f: F) -> ThreadPoolResult
     where
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        match self.sender.send(Msg::Dispatch(job)) {
+            Err(err) => Result::Err(Box::new(err)),
+            _ => Result::Ok(()),
+        }
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for _ in 0..self.workers.len() {
+            self.sender.send(Msg::Stop).unwrap();
+        }
+        for worker in &mut self.workers {
+            worker.join();
+        }
     }
 }
 
 struct Worker {
-    id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Msg>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {} got a job; executing.", id);
-            job.call_box();
+            let msg = receiver.lock().unwrap().recv().unwrap();
+            match msg {
+                Msg::Dispatch(job) => {
+                    println!("worker {} got a job: ", id);
+                    job.call_box();
+                }
+                Msg::Stop => {
+                    println!("worker {} stoped: ", id);
+                    break;
+                }
+            }
         });
-
-        Self { id, thread }
+        Self {
+            thread: Some(thread),
+        }
     }
+
+    fn join(&mut self) {
+        if let Some(thread) = self.thread.take() {
+            thread.join().unwrap();
+        }
+    }
+}
+
+enum Msg {
+    Dispatch(Job),
+    Stop,
 }
 
 type Job = Box<dyn FnBox + Send + 'static>;
