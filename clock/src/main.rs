79ed58a7c16879465d412dc645afb7e94abad945
nfukasawa <get.it.on.fuka@gmail.com>
2020-01-13 23:47:28 +0900

    use iced.

--- clock/src/main.rs
+++ clock/src/main.rs
@@ -1,22 +1,73 @@
 use chrono::Local;
+use iced::{
+    Align, Application, Column, Command, Container, Element, Length, Settings, Subscription, Text,
+};
 use std::time::{Duration, Instant};
-use tokio::prelude::*;
-use tokio::timer::Interval;
 
-use console::Term;
+pub fn main() {
+    Clock::run(Settings::default())
+}
 
-fn main() {
-    let term = Term::stdout();
+struct Clock {}
 
-    tokio::run(
-        Interval::new(Instant::now(), Duration::new(1, 0))
-            .for_each(move |_| {
-                let now = Local::now();
-                term.clear_screen().unwrap();
-                term.write_line(now.format("%F (%a) %T").to_string().as_str())
-                    .unwrap();
-                Ok(())
-            })
-            .map_err(|e| panic!("error: {:?}", e)),
-    );
+#[derive(Debug, Clone)]
+enum Message {
+    Tick(Instant),
+}
+
+impl Application for Clock {
+    type Message = Message;
+
+    fn new() -> (Self, Command<Message>) {
+        (Self {}, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Clock")
+    }
+
+    fn update(&mut self, _message: Message) -> Command<Message> {
+        Command::none()
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        every(Duration::from_secs(1)).map(Message::Tick)
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        let now = Text::new(Local::now().format("%F (%a) %T").to_string()).size(40);
+        let content = Column::new().align_items(Align::Center).push(now);
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .center_y()
+            .into()
+    }
+}
+
+fn every(duration: std::time::Duration) -> iced::Subscription<std::time::Instant> {
+    iced::Subscription::from_recipe(Every(duration))
+}
+
+struct Every(std::time::Duration);
+
+impl<H, I> iced_native::subscription::Recipe<H, I> for Every
+where
+    H: std::hash::Hasher,
+{
+    type Output = std::time::Instant;
+
+    fn hash(&self, state: &mut H) {
+        use std::hash::Hash;
+        std::any::TypeId::of::<Self>().hash(state);
+        self.0.hash(state);
+    }
+
+    fn stream(self: Box<Self>, _input: I) -> futures::stream::BoxStream<'static, Self::Output> {
+        use futures::stream::StreamExt;
+        async_std::stream::interval(self.0)
+            .map(|_| std::time::Instant::now())
+            .boxed()
+    }
 }
